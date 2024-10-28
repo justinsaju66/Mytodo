7f4ae1a33ee8de98a2dfee78fc6da8de10b1b24e
Justin Saju <justinsaju66@gmail.com>
2024-10-28 13:45:54 +0530

    Initial commit

---
+++ Mytodo/Mytodo/src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.reymar.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytodoApplication.class, args);
+	}
+
+}
